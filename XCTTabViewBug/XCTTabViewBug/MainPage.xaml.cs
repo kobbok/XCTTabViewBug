a92153136975a79f7f4e67246d8f2e9a5aad3f54
Martin Prelog <martin.prelog@adcsistemi.si>
2023-11-09 15:25:11 +0100

    Initial commit

---
+++ XCTTabViewBug/XCTTabViewBug/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace XCTTabViewBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
